b4c710bb37b9fa09d38785287b1b90f37967de02
Andresser17 <andresserserrano@gmail.com>
2021-12-22 21:22:57 -0400

    Added /api/fileanalyze endpoint and added multer middleware

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
+
+// Configure multer
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+// Add body-parser in every endpoint
+app.use(express.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.array("upfile"), (req, res) => {
+  const upfile = req.files[0];
 
-
+  res.json({
+    name: upfile.originalname,
+    type: upfile.mimetype,
+    size: upfile.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
